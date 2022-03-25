67aa673f8569783b75829dc8370c16567b9e04e3
Sergey Rubnikovich <sergey.rubnikovich@gmail.com>
2022-03-25 15:28:48 +0700

    It works

--- android/app/src/main/java/com/myd/MainApplication.java
+++ android/app/src/main/java/com/myd/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.rnfs.RNFSPackage;
--- src/components/PlaylistsStackNavigator.js
+++ src/components/PlaylistsStackNavigator.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PlaylistsScreen from "./screens/authorized/PlaylistsScreen";
-// import TracksScreen from "./screens/authorized/TracksScreen";
-// import LikesTracksScreen from "./screens/authorized/LikesTracksScreen";
+import TracksScreen from "./screens/authorized/TracksScreen";
+import LikesTracksScreen from "./screens/authorized/LikesTracksScreen";
 
 
 const PlaylistsStack = createNativeStackNavigator();
@@ -12,8 +12,8 @@ export default function PlaylistsStackNavigator() {
     return (
         <PlaylistsStack.Navigator>
             <PlaylistsStack.Screen name="Playlists" component={PlaylistsScreen} />
-            {/*<PlaylistsStack.Screen name="Tracks" component={TracksScreen} />*/}
-            {/*<PlaylistsStack.Screen name="Likes" component={LikesTracksScreen} />*/}
+            <PlaylistsStack.Screen name="Tracks" component={TracksScreen} />
+            <PlaylistsStack.Screen name="Likes" component={LikesTracksScreen} />
         </PlaylistsStack.Navigator>
     );
 }
